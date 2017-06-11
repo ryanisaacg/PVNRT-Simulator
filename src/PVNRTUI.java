86fee5bf978ae85d48c508d8ebe04350cc8f66d9
Ryguy9999 <ryancirincione@aol.com>
2017-06-11 13:37:08 -0400

    Now render pressure and temperature, added calculation integration

--- src/PVNRTUI.java
+++ src/PVNRTUI.java
@@ -34,7 +34,7 @@ public class PVNRTUI
 		pressureLabel = new JLabel(pressure + " Pa");
 		tempSlider = new JSlider(1, 500, 1);
 		volumeSlider = new JSlider(1, 1000, 1);
-		pressureSlider = new JSlider(1, 100000, 1);
+		pressureSlider = new JSlider(1, 1000, 1);
 		tempLock = new JCheckBox("Temperature Independent");
 		volumeLock = new JCheckBox("Volume Independent");
 		pressureLock = new JCheckBox("Pressure Independent");
@@ -47,6 +47,7 @@ public class PVNRTUI
 			
 			//Constructor
 			{
+				JPanel p = this;
 				this.add(tempLabel);
 				this.add(tempSlider);
 				this.add(tempLock);
@@ -62,6 +63,8 @@ public class PVNRTUI
 					{
 						temperature = tempSlider.getValue();
 						tempLabel.setText(temperature + " K");
+						PVNRT.update(p);
+						refresh();
 					}
 				});
 				volumeSlider.addChangeListener(new ChangeListener(){
@@ -69,17 +72,20 @@ public class PVNRTUI
 					{
 						volume = volumeSlider.getValue();
 						volumeLabel.setText(volume + " m^3");
+						PVNRT.update(p);
+						refresh();
 					}
 				});
 				pressureSlider.addChangeListener(new ChangeListener(){
 					public void stateChanged(ChangeEvent e)
 					{
 						pressure = pressureSlider.getValue();
-						pressureLabel.setText(pressure+ " Pa");
+						pressureLabel.setText(pressure+ " kPa");
+						PVNRT.update(p);
+						refresh();
 					}
 				});
 				
-				JPanel p = this;
 				Timer t = new Timer();
 				t.scheduleAtFixedRate(new TimerTask(){
 					public void run()
@@ -91,13 +97,53 @@ public class PVNRTUI
 				this.setPreferredSize(new Dimension(440, 400));
 			}
 			
+			public void refresh()
+			{
+				tempSlider.setValue((int)temperature);
+				volumeSlider.setValue((int)volume);
+				pressureSlider.setValue((int)pressure);
+				tempLabel.setText(temperature + " K");
+				volumeLabel.setText(volume + " m^3");
+				pressureLabel.setText(pressure+ " kPa");
+			}
+			
 			public void paintComponent(Graphics g)
 			{
 				super.paintComponent(g);
+				
+				//Thermometer
+				g.setColor(Color.black);
+				g.fillOval(320, 330, 15, 15);
+				g.fillRoundRect(324, 254, 8, 81, 4, 4);
+				g.setColor(new Color(100, 100, 100));
+				g.fillRect(310, 333, 12, 3);
+				g.setColor(Color.red);
+				g.fillOval(321, 331, 13, 13);
+				g.fillRoundRect(325, (int)(330 - temperature*75/500), 6, (int)(5+temperature*75/500), 3, 3);
+				
+				//Pressure Gauge
+				g.setColor(new Color(100, 100, 100));
+				g.drawLine(73, 334, 109, 334);
+				g.drawLine(71, 333, 109, 333);
+				g.drawLine(70, 332, 109, 332);
+				g.drawLine(69, 331, 109, 331);
+				g.drawLine(68, 330, 93, 330);
+				g.drawLine(67, 329, 94, 329);
+				g.drawLine(66, 328, 95, 328);
+				g.drawLine(65, 327, 96, 327);
+				g.drawLine(64, 326, 97, 326);
+				g.drawLine(63, 325, 98, 325);
+				g.setColor(Color.black);
+				g.drawOval(60, 295, 40, 40);
+				g.drawLine(80, 325, 80+(int)(22*(Math.cos(Math.toRadians(160 - pressure*140/1000)))),
+						325-(int)(22*(Math.sin(Math.toRadians(160 - pressure*140/1000)))));
+
+				//Container
 				g.drawLine(109, 149, 109, 350);
 				g.drawLine(109, 350, 310, 350);
 				g.drawLine(310, 350, 310, 149);
 
+				//Piston
 				g.setColor(new Color(100, 100, 100));
 				g.drawLine(110, (int)(350 - volume/5 - 1), 309, (int)(350 - volume/5 - 1));
 				g.fillRect(200, (int)(350 - volume/5 - 60), 20, 60);
